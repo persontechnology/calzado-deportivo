021d3652cea61c62b982c1543688decc88b95352
soy-software <soysoftware@gmail.com>
2020-06-13 13:34:19 -0500

    artisan command add

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // Artisan::call('cache:clear');
+    // Artisan::call('config:clear');
+    // Artisan::call('config:cache');
+    // Artisan::call('storage:link');
+    // Artisan::call('key:generate');
+    // Artisan::call('migrate:fresh --seed');
 });
 
 Auth::routes(['register' => false]);
