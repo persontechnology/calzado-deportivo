e19259f6fe63778e087fd78739e45efe2a53f9eb
soy-software <soysoftware@gmail.com>
2020-06-13 14:30:26 -0500

    prfile email

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,7 +143,7 @@
 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <i class="fas fa-user"></i> {{ Auth::user()->name }} </a>  <span class="caret"></span>
+                                    <i class="fas fa-user"></i> {{ Auth::user()->email }} </a>  <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
